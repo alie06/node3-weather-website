e39ba2706b96c4d546d5606caf9cbffa56f0b4b6
Gacutan <danilie.t.gacutan@accenture.com>
2021-08-29 22:00:23 +0800

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require ('request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=f8cb8d46d14e9209863380a7befc543c&query=' + latitude + ',' + longitude +'&units=f'
 
@@ -12,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
             callback(undefined, body.current.weather_descriptions[0] 
                 + ". It is currently " + body.current.temperature + 
                 " degrees out. It feels like "  + body.current.feelslike + 
-                " degrees out.")
+                " degrees out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
